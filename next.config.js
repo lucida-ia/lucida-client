a5778251d34f2d6ced5068ce6582937c0f4cf38c
jpbmoura <jopmoura49@gmail.com>
2025-06-13 11:26:45 -0300

    uplooad up to 100mb

--- next.config.js
+++ next.config.js
@@ -9,6 +9,11 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  api: {
+    bodyParser: {
+      sizeLimit: "100mb",
+    },
+  },
 };
 
 module.exports = nextConfig;
