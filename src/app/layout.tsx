1c1ce8a5d796fe826705f897f2a98079117529c0
jpbmoura <jopmoura49@gmail.com>
2025-07-02 22:39:16 -0300

    classes ui changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
           <ThemeProvider attribute="class" enableSystem>
             <TooltipProvider>
               <div className="flex min-h-screen flex-col">
-                <main className="flex-1">{children}</main>
+                <main className="flex-1 ">{children}</main>
               </div>
             </TooltipProvider>
             <Toaster />
