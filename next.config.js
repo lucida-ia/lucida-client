e0bb159014905fcc481d63283f945d94e1765720
jpbmoura <jopmoura49@gmail.com>
2025-08-06 21:20:03 -0300

    posthog

--- next.config.js
+++ next.config.js
@@ -9,6 +9,24 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  async rewrites() {
+    return [
+      {
+        source: '/ingest/static/:path*',
+        destination: 'https://us-assets.i.posthog.com/static/:path*',
+      },
+      {
+        source: '/ingest/:path*',
+        destination: 'https://us.i.posthog.com/:path*',
+      },
+      {
+        source: '/ingest/flags',
+        destination: 'https://us.i.posthog.com/flags',
+      },
+    ];
+  },
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true,
 };
 
 module.exports = nextConfig;
