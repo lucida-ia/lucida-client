1a6347dc238f7513a4c0ac133aff9f17bd6e6f47
João Pedro <jopmoura49@gmail.com>
2026-04-22 14:04:02 -0300

    clerk

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/nextjs";
 export default function SignInPage() {
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <SignIn signUpUrl="/sign-up" />
+      <SignIn signUpUrl="/sign-up" forceRedirectUrl="/dashboard" />
     </div>
   );
 } 
